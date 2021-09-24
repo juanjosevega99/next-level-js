6c16f921ab5c737e17271a3ad1b0a49db98e1fe7
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2021-09-24 11:49:04 -0500

    remove clg and add a second use

--- challenge-pager.js
+++ challenge-pager.js
@@ -4,10 +4,13 @@ function main () {
     const input1 = [2, 1, 5, 4]
     const missing1 = missingNumber(input1)
     console.log('The missing number is', missing1)
+    
+    const input2 = [2, 1, 5, 4]
+    const missing2 = missingNumber(input2)
+    console.log('The missing number is', missing2)
   }
   
   function missingNumber(arrayNumber) {
-    console.log(arrayNumber)
     const arrayLength = arrayNumber.length + 1
     const total = (arrayLength * (arrayLength + 1)) / 2
     let sum = 0
