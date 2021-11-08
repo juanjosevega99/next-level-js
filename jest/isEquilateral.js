a41ee7e48b617268d842631e6f185c9386060a97
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2021-11-08 15:23:30 -0500

    testing function isEquilateral

---
+++ jest/isEquilateral.js
@@ -0,0 +1,6 @@
+// export default function isEquilateral(a, b, c) {
+export default function isEquilateral(a, b, c) {
+  return a === b && a === c;
+}
+
+module.exports = isEquilateral;
---
+++ jest/isEquilateral.test.js
@@ -0,0 +1,32 @@
+// import isEquilateral from './isEquilateral';
+const isEquilateral = require('./isEquilateral');
+
+describe('.isEquilateral(a, b, c)', () => {
+  test('should return true when all sides are equal', () => {
+    const a = 1;
+    const b = 1;
+    const c = 1;
+
+    const actual = isEquilateral(a, b, c);
+
+    expect(actual).toEqual(true);
+  });
+  test('should return false when only 2 sides are equal', () => {
+    const a = 3;
+    const b = 1;
+    const c = 1;
+
+    const actual = isEquilateral(a, b, c);
+
+    expect(actual).toEqual(false);
+  });
+  test('should return false when there are no equal sides', () => {
+    const a = 3;
+    const b = 2;
+    const c = 1;
+
+    const actual = isEquilateral(a, b, c);
+
+    expect(actual).toEqual(false);
+  });
+});
