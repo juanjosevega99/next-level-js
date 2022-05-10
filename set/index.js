6b356b1e9c89fcec0493294d7cf8190e4b751099
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-05-10 09:24:21 -0500

    othe use case of set

--- set/index.js
+++ set/index.js
@@ -12,3 +12,15 @@ const frameWorks = [
 
 const frameWorksNotRepeated = [...new Set(frameWorks)];
 console.log(frameWorksNotRepeated);
+
+// How works set to add in string or object
+
+const set = new Set();
+
+set.add(1);
+set.add('Lydia');
+set.add({ name: 'Lydia' });
+
+for (let item of set) {
+  console.log(item + 2);
+}
